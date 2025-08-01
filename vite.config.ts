3604e418c479313d47703b0e63165955b243401d
Dhruv Shridhar <84215615+RAVOKERcodes@users.noreply.github.com>
2025-08-01 15:01:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE || '/',
+  base: process.env.VITE_BASE || '/portfolio',
   server: {
     host: "::",
     port: 8080,
