b19d65f3532e4d28ef992304525bad0c93b8136a
Dhruv Shridhar <84215615+RAVOKERcodes@users.noreply.github.com>
2025-08-01 16:17:33 +0530

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -16,7 +16,7 @@ const NotFound = () => {
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4 text-blue-700">404</h1>
         <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
-        <a href="/" className="text-blue-500 hover:text-blue-700 underline">
+        <a href="/portfolio/" className="text-blue-500 hover:text-blue-700 underline">
           Return to Home
         </a>
       </div>
