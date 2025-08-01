7a965b52b2be59cc39232cdb617bb562f1d2ec21
Dhruv Shridhar <84215615+RAVOKERcodes@users.noreply.github.com>
2025-08-01 14:50:39 +0530

    fix: Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.VITE_BASE || '/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
