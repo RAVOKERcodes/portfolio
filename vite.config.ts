6feb9ad147734c92f8d112e0938b0808b6309fae
Dhruv Shridhar <84215615+RAVOKERcodes@users.noreply.github.com>
2025-08-01 15:21:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: process.env.VITE_BASE || '/',
   server: {
@@ -13,6 +13,15 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '.',
+          rename: '404.html'
+        }
+      ]
+    }),
   ].filter(Boolean),
   resolve: {
     alias: {
